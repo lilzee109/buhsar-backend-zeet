6ccf3f2836c7b3e392a54df718fdbdbfa8d9bc20
Muhammad Safaat <paatlupi01@gmail.com>
2023-02-23 04:47:03 +0700

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.listen(port, () => { console.log(`Server Run ${port}`) })
